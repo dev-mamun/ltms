1902a18ea1b1f695280b0e0ae79f4aa93a1a6a19
Md.Abdullah al mamun <mamun1214@gmail.com>
2024-07-06 17:02:02 +0600

    update

--- 00-laravel-deploy.sh
+++ 00-laravel-deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer global require hirak/prestissimo
+#composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
 echo "generating application key..."
